fc594ba4a5af4beba008773d7b4d552e4f0b1314
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-10-23 08:22:37 +0530

    Time: 10 ms (41.87%), Space: 54.3 MB (68.88%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-      Arrays.sort(nums);
       int n = nums.length;
       
-      int expectedSum = (n * (n+1))/2;
-      int currSum = 0;
-      
+      int freq[] = new int[n+1];
       int ans[] = new int[2];
       
-      for(int i = 0; i < n-1; i++){
-        if(nums[i] == nums[i+1])
-        {
-          ans[0] = nums[i];
-        }
-        else{
-          currSum += nums[i];
-        }
+      for(int i = 0; i < n; i++){
+        freq[nums[i]]++;
       }
-      ans[1] = expectedSum - (currSum + nums[n-1]);
+      
+    for(int i = 1;i <= n; i++){
+      if(freq[i] > 1){
+        ans[0] = i;
+      }
+      else if(freq[i] == 0){
+        ans[1] = i;
+      }
+    }
       return ans;
     }
 }
