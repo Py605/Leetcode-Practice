b5d0ade6777580b28e48f79bcdddd72d39268681
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-10-27 13:25:31 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        
+        long ans[] = new long[n];
+        
+        Stack<Long> st = new Stack<>();
+        for(int i = n-1; i >= 0; i--){
+            long x = arr[i];
+            
+            while(!st.isEmpty()){
+                if(x < st.peek()){
+                    break;
+                }
+                else{
+                    st.pop();
+                }
+            }
+            
+            if(!st.isEmpty()){
+                ans[i] = st.peek();
+            }
+            else{
+                ans[i] = -1;
+            }
+            
+            st.push(x);
+        }
+        return ans;
+    } 
+}
