43481d2851352cabd38c1d502198e3b492fa6f73
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-10-31 07:24:25 +0530

    Time: 1 ms (93.83%), Space: 46 MB (29.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -3,19 +3,11 @@ class Solution {
     
     int m = matrix.length;
       int n = matrix[0].length;
-    HashMap<Integer,Integer> hm = new HashMap();
     
     for(int i = 0;i < m;i++){
       for(int j = 0;j < n; j++){
-        if(hm.containsKey(i-j)){
-          int value = hm.get(i-j);
-          if(value != matrix[i][j])
-            return false;
-        }
-        else{
-          hm.put(i-j,matrix[i][j]);
-        }
-        
+        if(i > 0 && j > 0 && matrix[i][j] != matrix[i-1][j-1])
+          return false;
       }
     }
     
