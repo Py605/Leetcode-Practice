b98add9f410bf4645273318e17c185d9c602b7d5
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-11-07 08:39:54 +0530

    Time: 4 ms (32.56%), Space: 39.1 MB (91.45%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,19 +1,10 @@
 class Solution {
     public int maximum69Number (int num) {
-        // Convert the input 'num' to a string builder 'numSB'.
-        StringBuilder numSB = new StringBuilder();
-        numSB.append(num);
-        
-        // Iterate over the string builer (from high to low).
-        for (int i = 0; i < numSB.length(); ++i) {
-            // If we find the first '6', replace it with '9' and break the loop.
-            if (numSB.charAt(i) == '6') {
-                numSB.setCharAt(i, '9');
-                break;
-            }
-        }
-        
-        // Convert the modified string builder to integer and return it.
-        return Integer.parseInt(numSB.toString());
+        // Convert the input 'num' to the string 'numString'.
+        String numString = "" + num;
+
+        // Use the built-in function to replace the first '6' with '9'.
+        // Return the integer converted from the modified 'numString'.
+        return Integer.parseInt(numString.replaceFirst("6", "9"));
     }
 }
