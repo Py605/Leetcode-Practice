6bada3b283cbfc696c5112f39ac94049ef0b637e
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-11-13 09:52:41 +0530

    Time: 5 ms (94.21%), Space: 43 MB (74.68%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,54 @@
 class Solution {
     public String reverseWords(String s) {
-      s = s.trim();
-      String c[] = s.split(" ",-1);
-      int last = c.length-1, first = 0;
-      
-      while(first < last){
-        if(c[first] != ""){
-          first++;
-        }else if(c[last] != ""){
-          last--;
+        char[] c= s.toCharArray();
+        reverse(c,0,c.length-1);
+        reverseWord(c);
+        return removeExtraSpace(c);
+    }
+    
+    private void reverse(char[] c,int i, int j){
+
+        while(j >= i){
+            char temp = c[i];
+            c[i] = c[j];
+            c[j] = temp;
+            
+            i++;
+            j--;
         }
-        else{
-          String temp = c[first];
-          c[first] = c[last];
-          c[last] = temp;
-          first++; last--;
+    }
+    
+    private void reverseWord(char[] c){
+        int i = 0;
+        int curr = 0;
+        boolean got = false;
+        while(i < c.length){
+            
+            if(c[i] == ' '){ i++;continue; }
+            
+            if(!got){
+                curr = i;
+                got = true;
+            }
+            
+            if(( i+1 == c.length || c[i+1] == ' ')){
+                reverse(c,curr,i);
+                got = false;
+            }
+            i++;
         }
-      }
-       s = "";
-      for(int i = c.length-1; i >= 0; i--){
-        if(c[i] != "")
-          s += c[i]+" ";  
-      }
-      return s.substring(0,s.length()-1);
+        
+    }
+    
+    private String removeExtraSpace(char[] a){
+         int i = 0, j = 0;
+      int n = a.length;
+       while (j < n) {
+           while (j < n && a[j] == ' ') j++;      
+           while (j < n && a[j] != ' ') a[i++] = a[j++]; 
+           while (j < n && a[j] == ' ') j++;             
+           if (j < n) a[i++] = ' ';                    
+         }
+        return new String(a).substring(0,i);
     }
 }
