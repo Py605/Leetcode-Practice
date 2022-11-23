913681b6e16e39d0e528d6422373d287b43a2f69
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-11-23 06:34:16 +0530

    Time: 30 ms (21.20%), Space: 48.1 MB (29.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,21 +1,16 @@
 class Solution {
   public boolean isValidSudoku(char[][] board) {
-      Set rowSet = new HashSet<>();
-      Set colSet = new HashSet<>();
-      Set grid = new HashSet<>();
-
-      for(int i = 0; i < board.length; i++) {
-          for(int j = 0; j < board[0].length; j++) {
-              if(board[i][j] == '.') {
-                  continue;
-              }
-
-              if(!rowSet.add(String.valueOf(board[i][j]) + i) || !colSet.add(String.valueOf(board[i][j]) + j) || !grid.add(String.valueOf(board[i][j]) + i/3 + "-" + j/3)) {
-                  return false;
-              }
-          }
+    Set seen = new HashSet();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                    return false;
+        }
     }
-    
     return true;
-  }
+}
 }
