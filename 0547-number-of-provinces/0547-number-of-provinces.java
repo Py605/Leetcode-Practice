aa2e957b1df7a0dfc03fee4ab5c6486a67630b2c
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-06-04 18:38:43 +0530

    Time: 1 ms (98.97%), Space: 44.3 MB (14.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,47 +1,25 @@
 class Solution {
-  ArrayList<ArrayList<Integer>> adj;
-  public int findCircleNum(int[][] isConnected) {
-    int n = isConnected.length;
-
-    adj = new ArrayList();
-
-    for(int i = 0; i <= n ;i++){
-      adj.add(new ArrayList());
-    }
-
-    // Now, connecting all nodes..
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j< n ; j++){
-        if(i != j && isConnected[i][j] == 1){
-          ArrayList<Integer> first = adj.get(i+1);
-          first.add(j+1);
-          adj.set(i+1,first);
+    public void dfs(int node, int[][] isConnected, boolean[] visit) {
+        visit[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visit[i]) {
+                dfs(i, isConnected, visit);
+            }
         }
-      }
     }
 
-    for(ArrayList<Integer> al: adj){
-      System.out.println(al);
-    }
-    int ans = 0;
-    boolean visited[] = new boolean[n+1];
-    for(int i = 1; i <= n; i++){
-      if(!visited[i]){
-        ans++;
-        dfs(i,visited);
-      }
-    }
-    return ans;
-  }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int numberOfComponents = 0;
+        boolean[] visit = new boolean[n];
 
-  public void dfs(int ind, boolean visited[]){
-    if(visited[ind]){
-      return ;
-    }
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                dfs(i, isConnected, visit);
+            }
+        }
 
-    visited[ind] = true;
-    for(int el : adj.get(ind)){
-      dfs(el,visited);
+        return numberOfComponents;
     }
-  }
 }
