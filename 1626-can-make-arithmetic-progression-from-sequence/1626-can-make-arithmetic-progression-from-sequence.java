9991ae0ec583d9fb7d9e7559c58eea31d11e2c87
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-06-14 10:24:17 +0530

    Time: 2 ms (81.07%), Space: 41 MB (33.57%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
@@ -3,25 +3,38 @@ class Solution {
         int minValue = Arrays.stream(arr).min().getAsInt();
         int maxValue = Arrays.stream(arr).max().getAsInt();
         int n = arr.length;
-        
-        if (maxValue - minValue == 0) {
-            return true;
-        }
-        
         if ((maxValue - minValue) % (n - 1) != 0) {
             return false;
         }
         
         int diff = (maxValue - minValue) / (n - 1);
-        Set<Integer> numberSet = new HashSet<>();
+        int i = 0;
         
-        for (int i = 0; i < n; i++) {
-            if ((arr[i] - minValue) % diff != 0) {
+        while (i < n) {
+            // If arr[i] is at the correct index, move on.
+            if (arr[i] == minValue + i * diff) {
+                i += 1;
+                
+            // If arr[i] doesn't belong to this arithmetic sequence, return false.
+            } else if ((arr[i] - minValue) % diff != 0) {
                 return false;
+                
+            // Otherwise, find the index j to which arr[i] belongs, swap arr[j] with arr[i].
+            } else {
+                int j = (arr[i] - minValue) / diff;
+                
+                // If we find duplicated elements, return False.
+                if (arr[i] == arr[j]) {
+                    return false;
+                }
+                
+                // Swap arr[i] with arr[j].
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            numberSet.add(arr[i]);
         }
         
-        return numberSet.size() == n;
+        return true;
     }
 }
