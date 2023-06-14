ef2f4cc17ead8ef869aae3c98cb82d1da4390536
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-06-14 08:58:48 +0530

    Time: 48 ms (54.19%), Space: 46.3 MB (94.32%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,25 +1,24 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int count = 0, n = grid.length;
-        
-        // Check each row r against each column c.
-        for (int r = 0; r < n; ++r) {
-            for (int c = 0; c < n; ++c) {
-                boolean match = true;
+        int count = 0;
+        int n = grid.length;
 
-                // Iterate over row r and column c.
-                for (int i = 0; i < n; ++i) {
-                    if (grid[r][i] != grid[i][c]) {
-                        match = false;
-                        break;
-                    }
-                }
-
-                // If row r equals column c, increment count by 1.
-                count += match ? 1 : 0;
-            }
+        // Keep track of the frequency of each row.
+        Map<String, Integer> rowCounter = new HashMap<>();
+        for (int[] row : grid) {
+            String rowString = Arrays.toString(row);
+            rowCounter.put(rowString, 1 + rowCounter.getOrDefault(rowString, 0));
         }
-        
+
+        // Add up the frequency of each column in map.
+        for (int c = 0; c < n; c++) {
+            int[] colArray = new int[n];
+            for (int r = 0; r < n; ++r) {
+                colArray[r] = grid[r][c];
+            }
+            count += rowCounter.getOrDefault(Arrays.toString(colArray), 0);
+        }
+
         return count;
     }
 }
