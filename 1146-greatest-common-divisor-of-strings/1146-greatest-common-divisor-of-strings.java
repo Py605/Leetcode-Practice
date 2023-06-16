c7d58e838c63bc53693823d195d155bc3bb9a63f
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-06-16 10:50:31 +0530

    Time: 1 ms (84.79%), Space: 41.1 MB (89.09%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,22 +1,20 @@
 class Solution {
-    public boolean valid(String str1, String str2, int k) {
-        int len1 = str1.length(), len2 = str2.length();
-        if (len1 % k > 0 || len2 % k > 0) {
-            return false;
+    public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
         } else {
-            String base = str1.substring(0, k);
-            return str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty();
-        }
+            return gcd(y, x % y);
+        }    
     }
     
-    
     public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        for (int i = Math.min(len1, len2); i >= 1; --i) {
-            if (valid(str1, str2, i)) {
-                return str1.substring(0, i);
-            }
+        // Check if they have non-zero GCD string.
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
-        return "";
+        
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
     }
 }
