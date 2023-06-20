8ac0cfee35f44732ba5b416a5b71d1c5f4c016fb
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-06-20 08:31:22 +0530

    Time: 1 ms (100%), Space: 45.5 MB (11.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,27 +1,18 @@
 class Solution {
   public void moveZeroes(int[] nums) {
-    int i = 0, j = 0, n = nums.length;
-
-    while(i < n && j < n){
-      // i will store non-zero elements....
-      while(i < n && nums[i] == 0){
-        i++;
-      }
-      // j will store zero elements index....
-      while(j < n && nums[j] != 0){
-        j++;
-      }
-      // now, swaping..
-      if(i >= n || j >= n)
-      return ;
-      
-      if(i < j){
-        i++;
-        continue;
-      }
-      int temp = nums[i]; 
-      nums[i] = nums[j];
-      nums[j] = temp;
+    int lastNonZeroFoundAt = 0;
+    // If the current element is not 0, then we need to
+    // append it just in front of last non 0 element we found. 
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt++] = nums[i];
+        }
+    }
+ 	// After we have finished processing new elements,
+ 	// all the non-zero elements are already at beginning of array.
+ 	// We just need to fill remaining array with 0's.
+    for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+        nums[i] = 0;
     }
   }
 }
