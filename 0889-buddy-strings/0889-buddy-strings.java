9f1bef9b8fc242b28b53f088d14331cfe395b238
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-07-03 08:33:30 +0530

    Time: 3 ms (65.16%), Space: 41.8 MB (43.2%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+      int n = s.length(), m = goal.length();
+      if(n != m || n == 1)
+        return false;
+
+      if(s.equals(goal)){
+        return minTwoElementsEqual(s);
+      }
+      
+      ArrayList<Integer> different = new ArrayList<>();
+      for(int i = 0; i < n; i++){
+        if(s.charAt(i) != goal.charAt(i)){
+          different.add(i);
+        }
+      }
+      if(different.size() != 2)
+        return false;
+      int i = different.get(0), j = different.get(1);
+        return s.charAt(i) == goal.charAt(j) && s.charAt(j) == goal.charAt(i);
+    }
+  public boolean minTwoElementsEqual(String s){
+    char arr[] = s.toCharArray();
+    Arrays.sort(arr);
+    
+    for(int i = 0;i < arr.length-1; i++){
+      if(arr[i] == arr[i+1])
+        return true;
+    }
+    return false;
+  }
+}
