db01035d51f1af67c9c8ea82ad97b6a7b8aabc31
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2023-09-04 09:50:44 +0530

    Time: 6 ms (5.68%), Space: 41.5 MB (16.5%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,24 +13,38 @@
  *     }
  * }
  */
-class Solution {
-    List<Integer> al;
+class Solution { 
+  List<Integer> al;
     public List<Integer> rightSideView(TreeNode root) {
-      al  = new ArrayList();
-      inorder(root,0,0);
+      al = new ArrayList();
+      buildQueue(root);
+
       return al;
     }
-  public void inorder(TreeNode root, int i, int j){
-    if(root == null)
-      return ;
-    
-    if(al.size() > j){
-      al.set(j,root.val);
+    public void buildQueue(TreeNode root){
+      if(root == null){
+        return ;
+      }
+      Queue<TreeNode> qu = new LinkedList();
+
+      qu.add(root);
+      while(!qu.isEmpty()){
+        List<Integer> temp = new ArrayList();
+
+        int size = qu.size();
+        for(int i = 0;i < size; i++){
+          TreeNode cur = qu.poll();
+          System.out.println(cur.val);
+
+          if(cur.left != null){
+            qu.add(cur.left);
+          }
+          if(cur.right != null){
+            qu.add(cur.right);
+          }
+          temp.add(cur.val);
+        }
+        al.add(temp.get(temp.size()-1));
+      }
     }
-    else{
-      al.add(j,root.val);
-    }
-    inorder(root.left, i-1,j+1);
-    inorder(root.right, i+1,j+1);
-  }
 }
